d44ceb8b0c249474a7c86ea4d2daf31da7894656
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2024-10-14 13:48:23 +0100

    Working on classes

--- Tutorials/Classes.kt
+++ Tutorials/Classes.kt
@@ -1,9 +1,13 @@
-package com.example.fundamentalskotlin.Tutorials
-
+// Classes
 fun main() {
+    // runSmartDevice()
+    // runCar()
+    runCall()
+}
+
+fun runSmartDevice() {
     val device = SmartDevice()
 
-    
     device.name = "Test"
     device.turnOn()
     device.turnOff()
@@ -18,6 +22,47 @@ class SmartDevice {
 
     fun turnOff() {
         println("Smart device is turned off.")
+    }
+}
+
+// Classes - get / set
+fun runCar() {
+    var carInstance = Car()
+
+    carInstance.speed = 2
+    carInstance.level = 1
+
+    carInstance.runCar()
+}
+
+class Car {
+    var speed = 1
+       get() = field
+       set(value) {
+          field = 2 * value
+       }
+
+    var level = 1
+	get() = field
+	set(value) {
+	  field = value
+        }
+
+    fun runCar() {
+	println("Speed is $speed and level is $level")
+    }
+}
+// Constructor
+
+fun runCall() {
+    val phone = Phone("Hamza", "+21222020202")
+
+    phone.call()
+}
+
+class Phone(val name: String, val number: String) {
+    fun call() {
+	println("Calling $name - $number ....")
     }
 }
 
